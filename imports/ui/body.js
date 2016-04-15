2d2f82c7785950a445888d421e3909e744a3a4da
Fernando L. Canizo <fernandocanizo@gmail.com>
2016-04-15 08:19:28 -0300

    Add jQuery-ui and implement drag&drop for products
    Also style products into TBootstrap labels

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -10,32 +10,42 @@ import './body.html';
 import './navbar.html';
 import './footer.html';
 
-Template.products.helpers({
-	products: [
-		{ "_id" : "PdTyLYLw3qTALvDPa", "name" : "aceite", "addedOn" : 1460495880090, "addedBy" : "admin" },
-		{ "_id" : "Lt6ohxwfc345hucgj", "name" : "azúcar", "addedOn" : 1460495880095, "addedBy" : "admin" },
-		{ "_id" : "nPqupCHFpqQfhSAgy", "name" : "orégano", "addedOn" : 1460495880098, "addedBy" : "admin" },
-		{ "_id" : "m2zJNHMi9mrTKbzrJ", "name" : "pelpa", "addedOn" : 1460495880099, "addedBy" : "admin" },
-		{ "_id" : "jrDgNHcTJ3Bx7cKNy", "name" : "rollos", "addedOn" : 1460495880100, "addedBy" : "admin" },
-		{ "_id" : "R5p8BQvD8nH5Nzivw", "name" : "sal", "addedOn" : 1460495880102, "addedBy" : "admin" },
-	]
-});
 
 Meteor.subscribe('products');
 Meteor.subscribe('pantry');
 
+////////////////////////////////////////////////////////////////////////////////
+// pantry
+////////////////////////////////////////////////////////////////////////////////
 Template.pantry.helpers({
 	products: function () {
 		return Pantry.find({isHere: true});
 	}
 });
 
+Template.pantry.onRendered(function () {
+	let templateInstance = this;
+	templateInstance.$('#pantry').droppable({
+		drop: (evt, ui) => {
+			let query = { _id: ui.draggable.data('id') };
+			let changes = { $set: { isHere: true }};
+			Products.update(query, changes);
+		}
+	});
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// toBuy
+////////////////////////////////////////////////////////////////////////////////
 Template.toBuy.helpers({
 	products: function () {
 		return Pantry.find({isHere: false});
 	}
 });
 
+////////////////////////////////////////////////////////////////////////////////
+// products
+////////////////////////////////////////////////////////////////////////////////
 Template.products.helpers({
 	products: function () {
 		return Products.find({});
@@ -50,4 +60,13 @@ Template.products.events({
 		// reset input box
 		event.target.inputProduct.value = "";
 	}
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// productListItem
+////////////////////////////////////////////////////////////////////////////////
+Template.productListItem.onRendered(function () {
+	let templateInstance = this;
+	// make all products draggable
+	templateInstance.$(".js-draggable").draggable();
 });
